591d2c43fb9fcffc9a902c7cc6546486efdb571b
ShUbHaM13M <shubham.heeralal@gmail.com>
2023-09-12 13:03:14 +0530

    Updated styling of Paragraph Input

--- src/app/components/create-question/answer-type/Paragraph.tsx
+++ src/app/components/create-question/answer-type/Paragraph.tsx
@@ -1,7 +1,14 @@
 import { Input } from "antd";
 
 const Paragraph = () => {
-  return <Input.TextArea disabled />;
+  return (
+    <Input.TextArea
+      placeholder="Long paragraph"
+      disabled
+      style={{ resize: "none" }}
+      allowClear
+    />
+  );
 };
 
 export default Paragraph;
